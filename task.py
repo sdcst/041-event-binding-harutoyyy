40f5f62810b828ddc297d94963b6f3eca4412413
harutoyyy <159074392+harutoyyy@users.noreply.github.com>
2024-06-05 12:36:54 -0700

    s

--- task.py
+++ task.py
@@ -13,9 +13,9 @@ l2 = tk.Label(win,text="This button has an event bound by a bind")
 # buttons b1 and b2 do the same
 # note that the callback for b1 is included in its definition
 # but the callback for b2 is in a separate command
-Lion =  tk.Button(win,text="Click to play",command="playsound") 
-snake =  tk.Button(win,text="Click to play",command="")
-Pig =  tk.Button(win,text="Click to play",command="")
+Lion =  tk.Button(win,text="Click to play",command="lion.mp3") 
+snake =  tk.Button(win,text="Click to play",command="snake.mp3")
+Pig =  tk.Button(win,text="Click to play",command="oink.mp3")
 Bear =  tk.Button(win,text="Click to play",command="")
 Yipee=  tk.Button(win,text="Click to play",command="")
 
@@ -23,6 +23,13 @@ Lion.bind("<Button>")
 
 
 l1.pack()
+l2.pack()
+
+Lion.pack()
+snake.pack()
+Pig.pack()
+Bear.pack()
+Yipee.pack()
 
 
 win.mainloop()
